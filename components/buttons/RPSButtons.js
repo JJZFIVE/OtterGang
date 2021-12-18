93a40615dc89a4ec729905944bb34cbe64c26f3b
Joe Zakielarz <joseph.zakielarz@duke.edu>
2021-12-18 01:22:37 -0500

    Update RPS Buttons

--- components/buttons/RPSButtons.js
+++ components/buttons/RPSButtons.js
@@ -1,23 +1,41 @@
 import Image from "next/image";
 import { ethers } from "ethers";
-import { useState } from "react";
-import Link from "next/link";
 import { useWeb3React } from "@web3-react/core";
-import { injected } from "../InjectedProvider";
 
 import rocklogo from "../../public/rocklogo.png";
 import paperlogo from "../../public/paperlogo.png";
 import scissorslogo from "../../public/scissorslogo.png";
-import {
-  otterdollarcontractaddress,
-  marketcontractaddress,
-  nftcontractaddress,
-} from "../../.config";
+import { otterdollarcontractaddress } from "../../.config";
 import OtterdollarABI from "../../artifacts/contracts/Otterdollar.sol/Otterdollar.json";
-import NFTABI from "../../artifacts/contracts/NFT.sol/NFT.json";
-import MarketplaceABI from "../../artifacts/contracts/Marketplace.sol/Marketplace.json";
 
-export function RockButton() {
+export function RockButton(props) {
+  const { library } = useWeb3React();
+
+  const playRock = async () => {
+    try {
+      const signer = library.getSigner();
+      const otterDollar = new ethers.Contract(
+        otterdollarcontractaddress,
+        OtterdollarABI.abi,
+        signer
+      );
+
+      const result = await otterDollar.playGame(0);
+      console.log(result);
+      props.setPlayed(true);
+      // find a way to get if you won
+      if (result == 0) {
+        props.setResult(0);
+      } else if (result == 1) {
+        props.setResult(1);
+      } else {
+        props.setResult(2);
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
   return (
     <button>
       <Image src={rocklogo} height="200" width="200"></Image>
@@ -25,7 +43,34 @@ export function RockButton() {
   );
 }
 
-export function PaperButton() {
+export function PaperButton(props) {
+  const { library } = useWeb3React();
+
+  const playPaper = async () => {
+    try {
+      const signer = library.getSigner();
+      const otterDollar = new ethers.Contract(
+        otterdollarcontractaddress,
+        OtterdollarABI.abi,
+        signer
+      );
+
+      const result = await otterDollar.playGame(1);
+      console.log(result);
+      props.setPlayed(true);
+      // find a way to get if you won
+      if (result == 0) {
+        props.setResult(0);
+      } else if (result == 1) {
+        props.setResult(1);
+      } else {
+        props.setResult(2);
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
   return (
     <button>
       <Image src={paperlogo} height="200" width="200"></Image>
@@ -33,7 +78,34 @@ export function PaperButton() {
   );
 }
 
-export function ScissorsButton() {
+export function ScissorsButton(props) {
+  const { library } = useWeb3React();
+
+  const playScissors = async () => {
+    try {
+      const signer = library.getSigner();
+      const otterDollar = new ethers.Contract(
+        otterdollarcontractaddress,
+        OtterdollarABI.abi,
+        signer
+      );
+
+      const result = await otterDollar.playGame(2);
+      console.log(result);
+      props.setPlayed(true);
+      // find a way to get if you won
+      if (result == 0) {
+        props.setResult(0);
+      } else if (result == 1) {
+        props.setResult(1);
+      } else {
+        props.setResult(2);
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
   return (
     <button>
       <Image src={scissorslogo} height="200" width="200"></Image>
@@ -41,10 +113,17 @@ export function ScissorsButton() {
   );
 }
 
-export function WimpOut() {
+export function PlayAgain(props) {
+  function playAgain() {
+    props.setPlayed(false);
+  }
+
   return (
-    <button className="bg-yellow-500 border-black px-4 py-2 border-2 rounded-lg text-black">
-      Wimp Out
+    <button
+      onClick={playAgain}
+      className="bg-yellow-600 px-4 py-2 rounded-md text-black border-2 border-black font-mono"
+    >
+      Play Again
     </button>
   );
 }
--- pages/play.js
+++ pages/play.js
@@ -7,10 +7,12 @@ import {
   RockButton,
   PaperButton,
   ScissorsButton,
-  WimpOut,
+  PlayAgain,
 } from "../components/buttons/RPSButtons";
 
 export default function Play() {
+  const [played, setPlayed] = useState(false);
+  const [result, setResult] = useState(0);
   const {
     account,
     activate,
@@ -23,20 +25,52 @@ export default function Play() {
     provider,
     setError,
   } = useWeb3React();
-  return (
-    <div className="flex flex-col items-center font-mono text-xl mt-20">
-      <p className="content-center text-3xl">Rock Paper Scissors</p>
-      <p className="content-center mt-8">
-        Your furry opponent has selected its move. Choose wisely.
-      </p>
-      <div className="flex justify-between w-1/2 mt-10">
-        <RockButton />
-        <PaperButton />
-        <ScissorsButton />
+
+  if (active) {
+    if (played == false) {
+      return (
+        <div className="flex flex-col items-center font-mono text-xl mt-20">
+          <p className="content-center text-3xl">Rock Paper Scissors</p>
+          <p className="content-center mt-8">
+            Your furry opponent has selected its move. Choose wisely.
+          </p>
+          <div className="flex justify-between w-1/2 mt-10">
+            <RockButton setPlayed={setPlayed} setResult={setResult} />
+            <PaperButton setPlayed={setPlayed} setResult={setResult} />
+            <ScissorsButton setPlayed={setPlayed} setResult={setResult} />
+          </div>
+        </div>
+      );
+    } else {
+      if (result == 0) {
+        return (
+          <div className="flex flex-col items-center font-mono text-xl mt-20">
+            <p>You lost :(</p>
+            <PlayAgain setPlayed={setPlayed} />
+          </div>
+        );
+      } else if (result == 1) {
+        return (
+          <div className="flex flex-col items-center font-mono text-xl mt-20">
+            <p>You won!</p>
+            <p className="text-md">Here's your otterdollar</p>
+            <PlayAgain setPlayed={setPlayed} />
+          </div>
+        );
+      } else {
+        return (
+          <div className="flex flex-col items-center font-mono text-xl mt-20">
+            <p>You tied...</p>
+            <PlayAgain setPlayed={setPlayed} />
+          </div>
+        );
+      }
+    }
+  } else {
+    return (
+      <div className="flex flex-col items-center font-mono text-xl mt-20">
+        <p className="content-center text-3xl">Connect your wallet to play</p>
       </div>
-      <div className="mt-10">
-        <WimpOut />
-      </div>
-    </div>
-  );
+    );
+  }
 }
